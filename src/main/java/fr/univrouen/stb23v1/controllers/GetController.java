701c888c6b6a9e1ce2b16ae4bc405ca535b3e6f8
CharlesFac <charlesnsofac@gmail.com>
2023-04-12 15:17:32 +0200

    Ajout PostTest Gestion requetes GET et modele

---
+++ src/main/java/fr/univrouen/stb23v1/controllers/GetController.java
@@ -0,0 +1,36 @@
+package fr.univrouen.stb23v1.controllers;
+
+import fr.univrouen.stb23v1.model.STB;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.*;
+
+import java.awt.*;
+
+@RestController
+public class GetController {
+
+     @GetMapping("/resume")
+    public String getListSTBinXML(){
+         return "Envoi de la liste des STB";
+     }
+
+     @GetMapping("/stbid")
+    public String getSTBinXML(
+         @RequestParam(value = "id") String texte) {
+         return ("Détail du contenu STB demandé" + texte);
+     }
+
+     @GetMapping("/test")
+    public String getSTBtestinXML(
+            @RequestParam(value = "id") String id,
+            @RequestParam(value = "titre") String title){
+
+         return ("Test :<br> id = " + id + "<br>titre = " + title);
+     }
+
+     @RequestMapping(value = "/xml", produces = MediaType.APPLICATION_XML_VALUE)
+    public @ResponseBody STB getXML() {
+         STB stb = new STB(123,"Test STB","2023-04-01T14:22:33");
+         return stb;
+     }
+}
